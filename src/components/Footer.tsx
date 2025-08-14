18bfd9e388e440cfe478f1405363ca665688736b
Granular RL <210986489+granular-rl@users.noreply.github.com>
2025-08-14 12:55:16 +0200

    Update website

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,24 +14,36 @@ const Footer: React.FC<FooterProps> = ({ githubUrl, googleScholarUrl, linkedInUr
     return (
         <footer className="pt-8 pb-4">
             <div className="container mx-auto w-full max-w-[90%] lg:max-w-4xl flex justify-between items-center text-normal text-base">
-                <div className="flex space-x-8">
-                    <span className="text-gray-800">
-                        &nbsp;
-                    </span>
+                {/* Left side: Website template text */}
+                <div className="flex items-center space-x-1" style={{ color: textColor }}>
+                    <span>Website template available on</span>
+                    {githubUrl && (
+                        <a
+                            href={"https://github.com/maltemosbach/project-page-template"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="flex items-center hover:underline"
+                            style={{ color: linkColor }}
+                        >
+                            GitHub <AiFillGithub size={16} className="ml-1" />
+                        </a>
+                    )}
                 </div>
+
+                {/* Right side: Social icons */}
                 <div className="flex space-x-8">
                     {githubUrl && (
-                        <a href={githubUrl} target="_blank" rel="noopener noreferrer" className={`no-underline-effect`} style={{color: linkColor}} aria-label="GitHub">
+                        <a href={githubUrl} target="_blank" rel="noopener noreferrer" className="no-underline-effect" style={{ color: linkColor }} aria-label="GitHub">
                             <AiFillGithub size={24} />
                         </a>
                     )}
                     {googleScholarUrl && (
-                        <a href={googleScholarUrl} target="_blank" rel="noopener noreferrer" className={`no-underline-effect`} style={{color: linkColor}} aria-label="Google Scholar">
+                        <a href={googleScholarUrl} target="_blank" rel="noopener noreferrer" className="no-underline-effect" style={{ color: linkColor }} aria-label="Google Scholar">
                             <AiOutlineGoogle size={24} />
                         </a>
                     )}
                     {linkedInUrl && (
-                        <a href={linkedInUrl} target="_blank" rel="noopener noreferrer" className={`no-underline-effect`} style={{color: linkColor}} aria-label="LinkedIn">
+                        <a href={linkedInUrl} target="_blank" rel="noopener noreferrer" className="no-underline-effect" style={{ color: linkColor }} aria-label="LinkedIn">
                             <AiFillLinkedin size={24} />
                         </a>
                     )}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,38 +37,42 @@ const IndexPage: React.FC<PageProps> = () => {
                     <Title>Interactive Shaping of Granular Media with Reinforcement Learning</Title>
 
                     {/* Authors */}
-                    {/*<div className="flex flex-wrap justify-center text-lg mb-2.5 mt-0 leading-none">*/}
-                    {/*    <Author website={""} firstAuthor={true} affiliations={"1"}>First Author</Author>*/}
-                    {/*    <Author website={""} firstAuthor={true} affiliations={"1"}>Second Author</Author>*/}
-                    {/*    <Author website={""} firstAuthor={false} lastAuthor={true} affiliations={"2"}>Last Author</Author>*/}
-                    {/*</div>*/}
+                    <div className="flex flex-wrap justify-center text-lg mb-2.5 mt-0 leading-none">
+                       <Author website={"https://www.hrl.uni-bonn.de/people/kreis"} firstAuthor={true} affiliations={""}>Benedikt Kreis</Author>
+                       <Author website={"https://maltemosbach.github.io/"} firstAuthor={false} affiliations={""}>Malte Mosbach</Author>
+                       <Author website={"https://www.linkedin.com/in/anny-ripke-678122339"} firstAuthor={false} affiliations={""}>Anny Ripke</Author>
+                       <Author website={"https://www.linkedin.com/in/iamehsanullah"} firstAuthor={false} affiliations={""}>M. Ehsan Ullah</Author>
+                       <Author website={"https://www.ais.uni-bonn.de/behnke/"} firstAuthor={false} affiliations={""}>Sven Behnke</Author>
+                       <Author website={"https://www.hrl.uni-bonn.de/people/bennewitz"} firstAuthor={false} lastAuthor={true} affiliations={""}>Maren Bennewitz</Author>
+                    </div>
 
-                    {/*<Thanks>* Indicates equal contribution</Thanks>*/}
+                    <Thanks>* Corresponding author</Thanks>
 
                     {/* Affiliations */}
                     <div className="flex flex-wrap justify-center text-base mb-12 mt-0 leading-none">
-                        Under Review
+                        University of Bonn
                     </div>
 
                     {/*/!* Links *!/*/}
-                    {/*<LinkGroup arxivUrl={""} pdfUrl={""} otherUrls={*/}
-                    {/*    [*/}
-                    {/*        ["/bibtex.txt", "BibTex"],*/}
-                    {/*        ["", "Code"],*/}
-                    {/*        ["", "Video"]*/}
-                    {/*    ]}*/}
-                    {/*/>*/}
+                    <LinkGroup
+                        arxivUrl=""
+                        pdfUrl=""
+                        otherUrls={[
+                            ["/bibtex.txt", "BibTex"],
+                            ["https://github.com/HumanoidsBonn/granular_rl", "Code"],
+                            // ["", "Video"]
+                        ]}
+                    >
+                    </LinkGroup>
                     
                     {/* Abstract */}
                     <Abstract>
                         Autonomous manipulation of granular media, such as sand, is crucial for applications in construction, excavation, and additive manufacturing.
-                        However, shaping granular materials presents unique challenges due to their high-dimensional configuration space and complex dynamics.
-                        Traditional rule-based approaches struggle with these complexities, requiring extensive engineering efforts.
+                        However, shaping granular materials presents unique challenges due to their high-dimensional configuration space and complex dynamics, where traditional rule-based approaches struggle without extensive engineering efforts.
                         Reinforcement learning (RL) offers a promising alternative by enabling agents to learn adaptive manipulation strategies through trial and error.
-                        Although RL has been successfully applied to rigid and deformable object manipulation, its application to granular media has received little attention.
-                        Thus, it has remained an open research question how to define the compact observations for the large configuration space and design an effective reward function.
-                        In this work, we present an RL framework that enables a robotic arm with a cubic end-effector to shape granular media into desired structures.
-                        Our results demonstrate the effectiveness of the proposed reward formulation for the training of visual policies that manipulate granular media including their real-world deployment.
+                        In this work, we present an RL framework that enables a robotic arm with a cubic end-effector to shape granular media into desired structures, outperforming two baseline approaches.
+                        We show the importance of compact observations and concise reward formulations for the large configuration space, validating our design choices with a rigorous ablation study of our feature extractor and the selection of RL algorithm.
+                        Our results demonstrate the effectiveness of the proposed approach for the training of visual policies that manipulate granular media including their real-world deployment.
                     </Abstract>
 
                     {/* Teaser Video */}
@@ -82,9 +86,9 @@ const IndexPage: React.FC<PageProps> = () => {
                     The videos show the simulated render view (left), the reconstructed height map (center), and the goal height map (right)."
                     items={[
                         { video: "/videos/sim_rectangle_combined.mp4", label: "Rectangle" },
-                        { video: "/videos/sim_trench_combined.mp4", label: "Trench" },
-                        { video: "/videos/sim_L_combined.mp4", label: "L Shape" },
-                        { video: "/videos/sim_fresco_combined.mp4", label: "Fresco Fragment" },
+                        { video: "/videos/sim_trench_combined.mp4", label: "Long rectangle" },
+                        { video: "/videos/sim_L_combined.mp4", label: "L-shape" },
+                        { video: "/videos/sim_fresco_combined.mp4", label: "Fresco fragment" },
                     ]}
                     scale={0.3}
                     />
@@ -94,7 +98,7 @@ const IndexPage: React.FC<PageProps> = () => {
                     text="Deployed to the real robotic system, our approach successfully creates the desired goal shape in the granular medium.
                     The video shows an external camera view (left), the reconstructed height map (center), and the goal height map (right)."
                     items={[
-                        { video: "/videos/real_trench_combined.mp4", label: "Trench" },
+                        { video: "/videos/real_trench_combined.mp4", label: "Long rectangle" },
                     ]}
                     scale={0.1}
                     />
@@ -112,15 +116,15 @@ const IndexPage: React.FC<PageProps> = () => {
                         },
                         {
                             urls: ['/models/trench_pcd_goal.ply', '/models/trench_pcd_achieved.ply'],
-                            label: 'Trench',
+                            label: 'Long rectangle',
                         },
                         {
                             urls: ['/models/L_pcd_goal.ply', '/models/L_pcd_achieved.ply'],
-                            label: 'L Shape',
+                            label: 'L-shape',
                         },
                         {
                             urls: ['/models/fragment_pcd_goal.ply', '/models/fragment_pcd_achieved.ply'],
-                            label: 'Fresco Fragment',
+                            label: 'Fresco fragment',
                         },
                         ]
                     }
@@ -148,14 +152,14 @@ const IndexPage: React.FC<PageProps> = () => {
                         <source src="/videos/teaser.mp4" type="video/mp4"/>
                     </video> */}
 
-                    {/* {<Citation/>} */}
+                    {<Citation/>}
 
                     </Article>
 
                 <Footer
-                    // githubUrl="https://github.com/your-profile"
-                    // googleScholarUrl="https://scholar.google.com/citations?user=example"
-                    // linkedInUrl="https://www.linkedin.com/in/your-profile"
+                    githubUrl="https://github.com/benediktkreis"
+                    googleScholarUrl="https://scholar.google.de/citations?hl=de&user=Ozj9MvYAAAAJ"
+                    linkedInUrl="https://www.linkedin.com/in/benedikt-kreis-235603164/"
                 />
             </main>
         </div>
